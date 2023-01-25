2038dd37341f11ce96e0d508555bead67c6308d9
Ka0gaMi <121442495+Ka0gaMi@users.noreply.github.com>
2023-01-26 01:52:57 +0200

    Added preloading screen

--- script.js
+++ script.js
@@ -151,3 +151,12 @@ function appendDecimal() {
     }
     currentActionScreen.textContent += "."
 }
+
+function delayPageLoad() {
+    myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("main").style.display = "block";
+  }
